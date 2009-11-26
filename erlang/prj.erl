0b9bf508ce25847e43ba07a3c0e26ed45f7c1b8a
pstrink@gmail.com <pstrink@gmail.com@211a6a70-b5ae-11de-92a3-373a1addbe3b>
2009-11-26 01:07:48 +0000

    added spaces in parameter lists and function calls for clarity.

--- erlang/prj.erl
+++ erlang/prj.erl
@@ -10,73 +10,73 @@
 -export([node_tell/3,node_rec/3,start/2,close/1,status/1,identify/0]).
 
 %%% Get Random Number
-getRand(MYNUM,NUMNODES) ->
+getRand(MYNUM, NUMNODES) ->
     random:seed(now()),
     RANDNUM = random:uniform(NUMNODES),
     if
       RANDNUM == MYNUM ->
-        getRand(MYNUM,NUMNODES);
+        getRand(MYNUM, NUMNODES);
       true ->
         "node" ++ integer_to_list(RANDNUM)
     end.
 
 %%% Tell Other Nodes
-node_tell(MYNUM,STATUS,NUMNODES) ->
+node_tell(MYNUM, STATUS, NUMNODES) ->
     if
       STATUS == "KnowAndTell" ->
           %%% if know pick random node and tell it the msg
-          MSGNODE = getRand(MYNUM,NUMNODES),
-          io:format("~p Tells ~p~n", [self(),MSGNODE]),
+          MSGNODE = getRand(MYNUM, NUMNODES),
+          io:format("~p Tells ~p~n", [self(), MSGNODE]),
           list_to_atom(MSGNODE) ! self(),
-          node_rec(MYNUM,STATUS,NUMNODES) ;
+          node_rec(MYNUM, STATUS, NUMNODES) ;
       true ->
-          node_rec(MYNUM,STATUS,NUMNODES)
+          node_rec(MYNUM, STATUS, NUMNODES)
     end.
 
 %%% Receive Messages
-node_rec(MYNUM,STATUS,NUMNODES) ->
+node_rec(MYNUM, STATUS, NUMNODES) ->
     receive
         close ->
            io:format("Stoping ~p~n", [self()]);
         status ->
-           io:format("Status ~p:~p~n", [self(),STATUS]),
-           node_rec(MYNUM,STATUS,NUMNODES);
+           io:format("Status ~p:~p~n", [self(), STATUS]),
+           node_rec(MYNUM, STATUS, NUMNODES);
         knew ->
           RANDNUM = random:uniform(),
-          io:format("I, ~p, Told someone who knew ~p~n", [self(),RANDNUM]),
+          io:format("I, ~p, Told someone who knew ~p~n", [self(), RANDNUM]),
           if
             RANDNUM < 0.05 ->
               io:format("I, (~p), quit.~n", [self()]),
-              node_rec(MYNUM,"KnowDontTell",NUMNODES);
+              node_rec(MYNUM, "KnowDontTell", NUMNODES);
             true ->
-              node_rec(MYNUM,STATUS,NUMNODES)
+              node_rec(MYNUM, STATUS, NUMNODES)
           end;
         learned ->
-            node_rec(MYNUM,STATUS,NUMNODES);
+            node_rec(MYNUM, STATUS, NUMNODES);
         NODE_ID ->
             if
               STATUS == "DontKnow" ->
-                %%%io:format("~p Learned From ~p~n", [self(),NODE_ID]),
+                %%%io:format("~p Learned From ~p~n", [self(), NODE_ID]),
                 NODE_ID ! learned,
-                node_rec(MYNUM,"KnowAndTell",NUMNODES);
+                node_rec(MYNUM, "KnowAndTell", NUMNODES);
               true ->
                 NODE_ID ! knew,
-                node_rec(MYNUM,STATUS,NUMNODES)
+                node_rec(MYNUM, STATUS, NUMNODES)
             end
     %%% if no matching message has arrived within ExprT milliseconds
     after 100 ->
         %%%io:format("Try Again ~p~n", [self()]),
-        node_tell(MYNUM,STATUS,NUMNODES)
+        node_tell(MYNUM, STATUS, NUMNODES)
     end.
 
 %%% Startup the System
-start(1,TOTAL) ->
-    register(node1, spawn(prj, node_tell, [1,"KnowAndTell",TOTAL]));
-start(N,TOTAL) ->
-    PID = spawn(prj, node_tell, [N,"DontKnow",TOTAL]),
+start(1, TOTAL) ->
+    register(node1, spawn(prj, node_tell, [1, "KnowAndTell", TOTAL]));
+start(N, TOTAL) ->
+    PID = spawn(prj, node_tell, [N, "DontKnow", TOTAL]),
     io:format("~p is node~p~n", [PID, N]),
     register(list_to_atom("node" ++ integer_to_list(N)), PID),
-    start(N-1,TOTAL).
+    start(N-1, TOTAL).
 
 %%% Shutdown the System
 close(0) ->
