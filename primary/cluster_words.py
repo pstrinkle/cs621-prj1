75a54bb57b57be41b80b44e2b0dd296a3efc996a
tri1@umbc.edu <tri1@umbc.edu@211a6a70-b5ae-11de-92a3-373a1addbe3b>
2012-02-01 15:51:41 +0000

    Improved algorithm.  Does the findmax() merge(), findmax() merge() loop over the centroids given a threshold.
    Currently the results aren't used -- the next step is to dump the information to a file... and maybe look at labeling mechanisms for centroids? -- terms with highest tf-idf.  Once this is done I can build a better focused stop word list and remove them.
    
    Also, currently I believe this leaves in singletons... they're typically removed.

--- primary/cluster_words.py
+++ primary/cluster_words.py
@@ -29,10 +29,29 @@ import Centroid
 def usage():
 	print "usage: %s <input file>" % sys.argv[0]
 
-def main():
-	# Weirdly in Python, you have free access to globals from within main().
+def findMax(centroids):
+	"""
+	Given a list of Centroids, compute the similarity of each pairing and return the
+	pair with the highest similarity, and their similarity score--so i don't have to 
+	re-compute.
+	"""
+	max_sim = 0.0
+	max_i = 0
+	max_j = 0
 
-	hourlyInterval = 0 # are we building hourly or daily histograms?
+	for i in xrange(0, len(centroids) - 1):
+		for j in xrange(0, len(centroids) - 1):
+			if i != j:
+				curr_sim = Centroid.similarity(centroids[i], centroids[j])
+				if curr_sim > max_sim:
+					max_sim = curr_sim
+					max_i = i
+					max_j = j
+
+	return (max_i, max_j, max_sim)
+
+def main():
+
 	totalTermCount = 0 # total count of all terms
 	cleanTweets = {}   # dictionary of the tweets by id as integer
 
@@ -50,8 +69,6 @@ def main():
 	with open(sys.argv[1], "r") as f:
 		tweets = f.readlines()
 
-	#print "tweets: %d" % len(tweets)
-
 	# ---------------------------------------------------------------------------
 	# Process tweets
 	for i in tweets:
@@ -68,9 +85,6 @@ def main():
 		# Do some cleanup
 		# Add this tweet to the collection of clean ones.
 		cleanTweets[info[0]] = TweetClean.cleanup(info[1])
-
-	# End of: "for i in tweets:"
-	# Thanks to python and not letting me use curly braces.
 
 	# ---------------------------------------------------------------------------
 	# Process the collected tweets
@@ -105,10 +119,6 @@ def main():
 	print "Total Count of Terms: %d" % totalTermCount
 	print "Unique Terms: %d" % len(docFreq)           # this is how many unique terms
 	print "How many Documents: %d" % len(docTermFreq) # this is how many days
-
-	#for id in docTermFreq:
-		#print "sizeof docTermFreq[%s]: %d" % (str(id), len(docTermFreq[id])) # this is how many unique terms were in that id
-		#print docTermFreq[id]
 	
 	# Calculate the inverse document frequencies.
 	invdocFreq = VectorSpace.calculate_invdf(len(docTermFreq), docFreq)
@@ -125,78 +135,29 @@ def main():
 	# docTermFreq    is the dictionary of term frequencies by date as integer
 	# docTfIdf       is similar to docTermFreq, but holds the tf-idf values
 
+	# ---------------------------------------------------------------------------
 	# Build Centroid List
 	centroids = []
 	
 	for doc, vec in docTfIdf.iteritems():
 		centroids.append(Centroid.Centroid(str(doc), vec))
 	
-	#print "sizeof(centroids): %d" % len(centroids)
-	#for cen in centroids:
-		#if cen.name not in docTfIdf:
-			#print "bad"
-	
+	# ---------------------------------------------------------------------------
+	# Merge centroids by highest similarity of at least threshold	
 	threshold = 0.025
 	deleted = False
 
-	# Not 100% satisfied with this method.  I should search for the maximum similarity
-	# and then repeat this process, as my c# program does.
 	while len(centroids) > 1:
-		print "while!"
-		# did we fully loop?
-		if i == (len(centroids) - 1) and j == (len(centroids) - 1):
+		i, j, sim = findMax(centroids)
+		
+		if sim >= threshold:
+			centroids[i].addVector(centroids[j].name, centroids[j].vectorCnt, centroids[j].centroidVector)
+			del centroids[j]
+			print "merged with sim: %.10f" % sim
+		else:
 			break
-		
-		deleted = False
-		
-		for i in xrange(0, len(centroids) - 1):
-			for j in xrange(0, len(centroids) - 1):
-				if i != j:
-					curr_sim = Centroid.similarity(centroids[i], centroids[j])
-					print "curr_sim: %f" % curr_sim
-					if curr_sim >= threshold:
-						print "above threshold merger, %s, %s" % (centroids[i].name, centroids[j].name)
-						centroids[i].addVector(centroids[j].name, centroids[j].vectorCnt, centroids[j].centroidVector)
-						del centroids[j]
-						deleted = True
-						break
-					else:
-						print "not above"
-			if deleted: # if inner loop deleted, then try again from outer loop.
-				print "should break to while"
-				break
 
-
-	# ---------------------------------------------------------------------------
-	# - Disabled code start -- just finds closest tweet and merges.
-	# Compute all similarities
-	#max_sim = 0.0
-	#max_nameA = ""
-	#max_nameB = ""
-	#max_i = 0
-	#max_j = 0
-
-	#for i in xrange(0, len(centroids) - 1):
-		#for j in xrange(0, len(centroids) - 1):
-			#if i != j:
-				#curr_sim = Centroid.similarity(centroids[i], centroids[j])
-				#if curr_sim > max_sim:
-					#max_sim = curr_sim
-					#max_nameA = centroids[i].name
-					#max_nameB = centroids[j].name
-					#max_i = i
-					#max_j = j
-				#print "similarity %f" % curr_sim
-	
-	#print "%f\n%s (%d) v %s (%d)" % (max_sim, max_nameA, max_i, max_nameB, max_j)
-	#print "'%s' -- '%s'" % (cleanTweets[max_nameA], cleanTweets[max_nameB])
-	#print "i: %f\nj: %f" % (centroids[max_i].length, centroids[max_j].length)
-
-	#centroids[max_i].addVector(centroids[max_j].name, 1, centroids[max_j].centroidVector)
-	#del centroids[max_j]
-	
-	#print "merged: %f\nmerged: '%s'" % (centroids[max_i].length, centroids[max_i].name)
-	# - End Disabled code region.
+	print "len(centroids): %d" % len(centroids)
 
 	# ---------------------------------------------------------------------------
 	# Done.
