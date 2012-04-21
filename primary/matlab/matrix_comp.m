8b9066fd73ce93a844086b1eba4f84bd356f6d98
tri1@umbc.edu <tri1@umbc.edu@211a6a70-b5ae-11de-92a3-373a1addbe3b>
2012-04-21 18:50:32 +0000

    my version of the script.

--- primary/matlab/matrix_comp.m
+++ primary/matlab/matrix_comp.m
@@ -1,34 +1,57 @@
-% Matrix completion demo
+% Matrix completion
+%
+% Given a matrix X1, we're going to say that it's ridiculously sparse, but
+% there are no zero columns or rows.  And instead of feeding the program some
+% random sampling, this feeds all non-zero entries and attempts to solve the
+% zero entries.
+%
+% Think the Netflix problem.
+%
+% A matrix of movies x users where each cell value is their rating.  So the goal
+%  is to fill the zero values in with non-zero values.  =)
 
-%rng(234923);    % for reproducible results
-X            = x1
-[rows, cols] = size(X)
-N            = rows * cols
-r            = 2;        % the rank of the matrix
-nSamples     = N / 2; % number of observed entries
+load 1.tab;
+% Let X1 be the matrix.
 
-% For this demo, we will use a matrix with integer entries
-% because it will make displaying the matrix easier.
-iMax         = 5;
+% Let “Omega” be the set of observed entries
 
-%Let “Omega” be the set of observed entries
-
-rPerm   = randperm(N); % use "randsample" if you have the stats toolbox
-omega   = sort( rPerm(1:nSamples) );
+% size(matrix_name) returns [row, col]
+M = size(X1);
 
 % Show the observed data
-Y = nan(N);
+Y = nan(size(X1)); % This will fill Y with NaN's as a matrix NxN.
+
+%
+% NaN(M,N) or NaN([M,N]) is an M-by-N matrix of NaNs.
+%
+% M := row count
+% N := col count
+%
+
+%
+% This will take the number in X and set it in Y for each value in omega, which
+% is a list j = [1, 2, 4, 5, 6]
+% Y(j) = X(j) would set Y(1) = X(1), Y(2) = X(2), Y(4) = X(4), 
+% leaving Y(3) == NaN
+%
+% What is non-obvious is that the indices go from 0th column, 0th row down to 
+% 1st row, 0th column, so top to bottom, left to right.
+omega = find(X1);
 Y(omega) = X(omega);
 disp('The "NaN" entries represent unobserved values');
 disp(Y)
 
+% So what I would want here is to get a list of entries that are non-zero.
+% ind = find(X) locates all nonzero elements of array X, and returns the linear 
+% indices of those elements in vector ind.
+
 % Run the completion code
-observations = X(omega);    % the observed entries
+observations = X(omega);    % the observed entries <-- amusingly my entries.
 mu           = .001;        % smoothing parameter
 
 % The solver runs in seconds
 tic
-Xk = solver_sNuclearBP( {row,col,omega}, observations, mu );
+Xk = solver_sNuclearBP( {M(1),M(2),omega}, observations, mu );
 toc
 
 % Show the recovered data
@@ -37,7 +60,7 @@ disp('Recovered matrix (rounding to nearest .0001):')
 disp( round(Xk*10000)/10000 )
 % and for reference, here is the original matrix:
 disp('Original matrix:')
-disp( X )
+disp(X1)
 
 % The relative error (without the rounding) is quite low:
-fprintf('Relative error, no rounding: %.8f%%\n', norm(X-Xk,'fro')/norm(X,'fro')*100 );
+%fprintf('Relative error, no rounding: %.8f%%\n', norm(X-Xk,'fro')/norm(X,'fro')*100 );
